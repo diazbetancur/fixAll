8772ccc6dd85f5ed45e0fd852c339d624d7c715a
Juanes <jesteban.2741@gmail.com>
2021-04-13 21:14:05 -0500

    NavigatioRegisterForm

--- components/account/RegisterForm.js
+++ components/account/RegisterForm.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import RegisterForm from '../../components/account/RegisterForm'
 
-export default function Register() {
+export default function RegisterForm() {
     return (
         <View>
-            <RegisterForm/>
+            <Text>RegisterForm</Text>
         </View>
     )
 }
--- navigations/AccountStack.js
+++ navigations/AccountStack.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 import Account from '../screens/account/Account'
 import Login from '../screens/account/Login'
+import Register from '../screens/account/Register'
 
 const Stack = createStackNavigator()
 
@@ -20,6 +21,11 @@ export default function AccountStack() {
                 component = {Login}
                 options ={{title : "Iniciar sesion"}}
             />
+            <Stack.Screen
+            name = "register"
+            component = {Register}
+            options ={{title : "Registrar"}}
+        />
         </Stack.Navigator>
     )
 }
