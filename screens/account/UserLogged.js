13d529fef5f5f19b26c44815c1e7d7eff060ab80
Juanes <jesteban.2741@gmail.com>
2021-04-14 21:43:43 -0500

    button Close Sesion

--- screens/account/UserLogged.js
+++ screens/account/UserLogged.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React , {useState, useRef , useEffect}from 'react'
 import { Button } from 'react-native-elements'
-import { View, Text } from 'react-native'
+import { View, Text ,StyleSheet} from 'react-native'
 import {useNavigation} from '@react-navigation/native'
 import { closeSession } from '../../utils/actions'
 
+
 export default function UserLogged() {
-
-    const navigation =useNavigation()
-
     return (
         <View>
             <Text>UserLogged</Text>
+            <Text>Options</Text>
             <Button
-                title ="cerrar sesion"
+                title ="Cerrar sesion"
+                containerStyle = {styles.btnContainer}
+                buttonStyle = {styles.closeSesionButton}
                 onPress = {() => {
                     closeSession()
                     navigation.navigate("search")
@@ -21,3 +22,14 @@ export default function UserLogged() {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    btnContainer:{
+        marginTop: 20,
+        width: "85%",
+        alignSelf: "center"
+    },
+    closeSesionButton:{
+        backgroundColor:"#cf2b2d"
+    }
+})
