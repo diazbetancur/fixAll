ef7ccd3a31912a9277c0393e0af479450402408b
Juanes <jesteban.2741@gmail.com>
2021-04-15 22:01:20 -0500

    Default Picture

---
+++ components/account/InfoUser.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Alert } from 'react-native'
+import { Avatar } from 'react-native-elements'
+
+import { updateProfile, uploadImage } from '../../utils/actions'
+import { loadImageFromGalery } from '../../utils/helpers'
+
+export default function InfoUser(user , setLoading , setLoadingText) {
+
+
+    const [photoUrl, setPhotoUrl] = useState(user.photoURL)
+    const changePhoto = async() =>{
+        const result = await loadImageFromGalery([1,1])
+        if(!result.status){
+            return
+        }
+        setLoadingText("Actualizando imagen...")
+        setLoading(true)
+        const resultUploadImage = await uploadImage(result.image, "avatars", user.uid)
+        console.log(resultUploadImage)
+        if(!resultUploadImage.statusResponse){
+            setLoading(false)
+            Alert.alert("Ha ocurrido un error al almacenar la foto de perfil")
+            return
+        }
+        const resultUpdateProfile = await updateProfile({photoURL: resultUploadImage.url})
+        
+        setLoading(true)
+        if(resultUpdateProfile.statusResponse){
+            setPhotoUrl(resultUploadImage.url)
+        }else{
+            Alert.alert("Ha ocurrido un error al actualizar la fot de perfil.")
+        }
+        
+    }
+
+    return (
+        <View style = {styles.container}>
+            <Avatar
+                
+                size = "large"
+                onPress = {changePhoto}
+                source = {
+                    photoUrl ? {uri : photoUrl} : require("../../assets/defaultPhoto.jpg")
+                }
+            />
+            <View style ={styles.infoUser}>
+                <Text style= {styles.displayName}>
+                    {
+                        user.displayName ? user.displayName : "Anonimo"
+                    }
+                </Text>
+                <Text>
+                    {
+                        user.email
+                    }
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        backgroundColor: "#f9f9f9",
+        paddingVertical:30
+    },
+    infoUser:{
+        marginLeft: 20
+    },
+    displayName: {
+        fontWeight: "bold",
+        paddingBottom: 5
+    }
+
+})
--- screens/account/UserLogged.js
+++ screens/account/UserLogged.js
@@ -2,34 +2,72 @@ import React , {useState, useRef , useEffect}from 'react'
 import { Button } from 'react-native-elements'
 import { View, Text ,StyleSheet} from 'react-native'
 import {useNavigation} from '@react-navigation/native'
-import { closeSession } from '../../utils/actions'
+import { closeSession , getCurrentUser} from '../../utils/actions'
+import Toast from "react-native-easy-toast"
+import Loading from '../../components/Loading'
+import InfoUser from '../../components/account/InfoUser'
 
 
 export default function UserLogged() {
+    const toastRef = useRef()
+    const navigation =useNavigation()
+
+    const [loading, setLoading] = useState(false)
+    const [loadingText, setLoadingText] = useState("")
+    const [user, setUser] = useState(null)
+    const [reloadUser, setReloadUser] = useState(false)
+
+    useEffect(() => {
+        setUser(getCurrentUser())
+        setReloadUser(false)
+    }, [reloadUser])
+
     return (
-        <View>
-            <Text>UserLogged</Text>
-            <Text>Options</Text>
+        <View style = {styles.container}>
+            {
+                user && (
+                    <View>
+                        <InfoUser 
+                            user = {user} 
+                            setLoading ={setLoading} 
+                            setLoadingText = {setLoadingText}
+                        />
+                    </View>
+                )
+            }
+           
             <Button
-                title ="Cerrar sesion"
-                containerStyle = {styles.btnContainer}
-                buttonStyle = {styles.closeSesionButton}
+                title = "cerrar sesion"
+                buttonStyle = {styles.btnCloseSession}
+                titleStyle= {styles.btnCloseSessionTitle}
                 onPress = {() => {
                     closeSession()
-                    navigation.navigate("search")
+                    navigation.navigate("restaurants")
                 }}
             />
+
+            <Toast ref = {toastRef} position = "center" opacity = {0.9} />
+            <Loading isVisible = {loading} text = {loadingText}/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    btnContainer:{
-        marginTop: 20,
-        width: "85%",
-        alignSelf: "center"
+    container: {
+        minHeight: "100%",
+        backgroundColor: "#f9f9f9"
     },
-    closeSesionButton:{
-        backgroundColor:"#cf2b2d"
+    btnCloseSession:{
+        marginTop: 30,
+        borderRadius: 5,
+        backgroundColor: "#FFFFFF",
+        borderTopWidth: 1,
+        borderTopColor: "#442484",
+        borderBottomWidth: 1,
+        borderBottomColor: "#442484",
+        paddingVertical: 10
+    },
+    btnCloseSessionTitle: {
+        color: "#442484"
     }
 })
