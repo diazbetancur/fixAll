bb9a99f6abe14b3a03372a92170eb68eb4fb19fa
Juanes <jesteban.2741@gmail.com>
2021-04-16 22:26:20 -0500

    Change Picture

--- components/account/InfoUser.js
+++ components/account/InfoUser.js
@@ -1,16 +1,45 @@
 import React, {useState} from 'react'
 import { StyleSheet, Text, View, Alert } from 'react-native'
 import { Avatar } from 'react-native-elements'
+import { loadImageFromGalery } from '../../utils/helpers'
+import {uploadImage , updateProfile} from '../../utils/actions'
 
 export default function InfoUser({user , setLoading , setLoadingText}) {
 
     const [photoUrl, setPhotoUrl] = useState(user.photoURL)
 
+    const changePhoto = async() =>{
+        const result = await loadImageFromGalery([1,1])
+        if(!result.status){
+            return
+        }
+        setLoadingText("Actualizando imagen...")
+        setLoading(true)
+        const resultUploadImage = await uploadImage(result.image, "avatars", user.uid)
+        console.log(resultUploadImage)
+        if(!resultUploadImage.statusResponse){
+            setLoading(false)
+            Alert.alert("Ha ocurrido un error al almacenar la foto de perfil")
+            return
+        }
+
+        const resultUpdateProfile = await updateProfile({photoURL: resultUploadImage.url})
+        console.log(resultUpdateProfile)
+        setLoading(true)
+        if(resultUpdateProfile.statusResponse){
+            setPhotoUrl(resultUploadImage.url)
+            setLoading(false)
+        }else{
+            Alert.alert("Ha ocurrido un error al actualizar la fot de perfil.")
+        }
+        
+    }
+
     
     return (
         <View style = {styles.container}>
             <Avatar
-                
+                onPress = {changePhoto}
                 size = "large"
                 
                 source = {
