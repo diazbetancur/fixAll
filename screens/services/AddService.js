e3dce331d71fad9a674412d20fa2930d5e3ac570
Juanes <jesteban.2741@gmail.com>
2021-04-18 12:13:21 -0500

    correction account

--- screens/services/AddService.js
+++ screens/services/AddService.js
@@ -1,6 +1,6 @@
 import React , { useRef, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import AddServiceForm from '../../components/Services/AddServiceForm'
+import AddServiceForm from '../../components/services/AddServiceForm'
 import Toast from 'react-native-easy-toast'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import Loading from '../../components/Loading'
