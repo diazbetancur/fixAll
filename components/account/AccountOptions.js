64ccc1a809e46523fa8a8e2223855881740d1106
Juanes <jesteban.2741@gmail.com>
2021-04-17 19:35:28 -0500

    Change Password

--- components/account/AccountOptions.js
+++ components/account/AccountOptions.js
@@ -5,6 +5,7 @@ import { Icon, ListItem } from 'react-native-elements'
 import Modal from '../Modal'
 import ChangeDisplayNameForm from './ChangeDisplayNameForm'
 import ChangeEmailForm from './ChangeEmailForm'
+import ChangePasswordForm from './ChangePasswordForm'
 
 
 export default function AccountOptions({user , toastRef , setReloadUser}) {
@@ -69,7 +70,10 @@ export default function AccountOptions({user , toastRef , setReloadUser}) {
                 break;
             case "password":
                 setRenderComponet(
-                    <Text>pass</Text>
+                    <ChangePasswordForm
+                        setShowModal= {setShowModal}
+                        toastRef = {toastRef}
+                    />
                 )
                 break;
             
