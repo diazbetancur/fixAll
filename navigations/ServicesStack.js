1415820a0d4398e4e3edff951cca7dffa76de3c2
diazbetancur <49962109+diazbetancur@users.noreply.github.com>
2021-04-18 11:05:58 -0500

    Create Service

--- navigations/ServicesStack.js
+++ navigations/ServicesStack.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
 
-import Services from '../screens/service/Services'
+import Services from '../screens/services/Services'
+import AddService from '../screens/services/AddService'
 
 const Stack = createStackNavigator()
 
@@ -10,9 +11,14 @@ export default function ServicesStack() {
     return (
         <Stack.Navigator>
             <Stack.Screen
-                name = "services"
-                component = {Services}
-                options = {{title : "Servicios"}}
+                name="services"
+                component={Services}
+                options={{ title: "Servicios" }}
+            />
+            <Stack.Screen
+                name="add-service"
+                //component={AddService}
+                options={{ title: "Servicios" }}
             />
         </Stack.Navigator>
     )
--- screens/services/Services.js
+++ screens/services/Services.js
@@ -1,12 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Icon } from 'react-native-elements'
+import { useFocusEffect } from '@react-navigation/native'
+import firebase from 'firebase/app'
 
-export default function Services() {
+import { getRestaurants } from '../../utils/actions'
+import Loading from '../../components/Loading'
+import { ScreenContainer } from 'react-native-screens'
+
+export default function Services({ navigation }) {
+    const [user, setUser] = useState(null)
+    const [restaurants, setRestaurants] = useState([])
+    const [startRestaurant, setStartRestaurant] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    if (user !== null) {
+        return <Loading isVisible={true} text="Cargando... " />
+    }
+
     return (
-        <View>
-            <Text>Services</Text>
+        <View style={styles.viewBody}>
+            <Text style={styles.text}>¿Que tipo de Servicio esta Ofreciendo ?</Text>
+            {
+                user === null && (
+                    <ScreenContainer style={styles.container}>
+                        <Text style={styles.textService}>Servicio en Sitio</Text>
+                        <Icon
+                            type="material-community"
+                            name="store-outline"
+                            color="#da5252"
+                            reverse
+                            containerStyle={styles.btnContainerSite}
+                            onPress={() => navigation.navigate("add-service")}
+                        /></ScreenContainer>
+                )
+            }
+            {
+                user === null && (
+                    <ScreenContainer style={styles.container}>
+                        <Text style={styles.textService} >Servicio a Domicilio</Text>
+                        <Icon
+                            type="material-community"
+                            name="moped"
+                            color="#da5252"
+                            reverse
+                            containerStyle={styles.btnContainerDelivery}
+                            onPress={() => navigation.navigate("add-service")}
+                        /></ScreenContainer>
+                )
+            }
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    btnContainerSite: {
+
+        bottom: 0,
+        shadowColor: "black",
+        shadowOffset: { width: 34, height: 34 },
+        shadowOpacity: 0.5,
+    },
+    btnContainerDelivery: {
+        shadowColor: "black",
+        shadowOffset: { width: 24, height: 24 },
+        shadowOpacity: 0.5,
+        margin: 10,
+        flexDirection: "row"
+
+    },
+    viewBody: {
+        flex: 1
+    },
+    text: {
+        fontSize: 30,
+        textAlign: "center",
+        color: "#f0cc20"
+    },
+    textService: {
+        fontSize: 25,
+        textAlign: "center",
+        color: "#f0cc20",
+        margin: 5
+    },
+    container: {
+        alignItems: "center",
+        marginTop: "33%"
+    }
+})
